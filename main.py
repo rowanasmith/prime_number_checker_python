8e6b2e2f0aa1ef9cae52202c4d263e8b7a7d06b8
rowanasmith <45500732+rowanasmith@users.noreply.github.com>
2022-02-15 05:59:03 +0000

    program functions

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 #Write your code below this line 👇
 
+def prime_checker(number):
+  prime_number = True
+  for numeral in range(2, number-1):
+    if (number % numeral) == 0:
+      prime_number = False
+  if prime_number is True:
+    print(f"It's a prime number.")
+  if prime_number is False:
+    print(f"It's not a prime number.")
 
 
 
